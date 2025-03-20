4907ea4b111485556da826665d2ad47d02f6a113
René Majewski <rene.majewski@gmx.de>
2025-03-20 17:48:05 +0100

    Create module

---
+++ src/module.ts
@@ -0,0 +1,10 @@
+import { defineNuxtModule } from '@nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    "name": "@rene-majewski/website-module-kreativ-register"
+  },
+  setup() {
+    console.debug("📦 @rene-majewski/website-module-kreativ-register geladen!")
+  }
+})
