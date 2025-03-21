936b7934a365d69a65bee0b703c1442f29f389c9
René Majewski <rene.majewski@gmx.de>
2025-03-21 11:55:50 +0100

    wip(lint): Config eslint and prettier

--- src/module.ts
+++ src/module.ts
@@ -1,29 +1,29 @@
-import { addComponentsDir, createResolver, defineNuxtModule, extendPages } from '@nuxt/kit'
+import { addComponentsDir, createResolver, defineNuxtModule, extendPages } from '@nuxt/kit';
 
 export default defineNuxtModule({
   meta: {
-    "name": "@rene-majewski/website-module-kreativ-register",
-    "configKey": "moduleKreativRegister",
-    "compatibility": {
-      "nuxt": ">=3.16.0"
-    }
+    name: '@rene-majewski/website-module-kreativ-register',
+    configKey: 'moduleKreativRegister',
+    compatibility: {
+      nuxt: '>=3.16.0',
+    },
   },
   defaults: {},
   setup() {
-    console.info("📦 @rene-majewski/website-module-kreativ-register will be loaded")
+    console.info('📦 @rene-majewski/website-module-kreativ-register will be loaded');
 
-    const resolver = createResolver(import.meta.url)
+    const resolver = createResolver(import.meta.url);
 
-    addComponentsDir({path: resolver.resolve('components')})
+    addComponentsDir({ path: resolver.resolve('components') });
 
     extendPages((pages) => {
       pages.push({
-        name: "kreativ-register",
-        path: "/kreativ-register",
-        file: resolver.resolve("runtime/pages/index.vue")
-      })
-    })
+        name: 'kreativ-register',
+        path: '/kreativ-register',
+        file: resolver.resolve('runtime/pages/index.vue'),
+      });
+    });
 
-    console.info(" ✓ has been loaded")
-  }
-})
+    console.info(' ✓ has been loaded');
+  },
+});
