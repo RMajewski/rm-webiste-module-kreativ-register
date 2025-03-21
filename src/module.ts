9ff35591f77128bc5cd0539226c05012218c1c11
René Majewski <rene.majewski@gmx.de>
2025-03-21 11:14:17 +0100

    wip(Pages): Add pages

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,29 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { addComponentsDir, createResolver, defineNuxtModule, extendPages } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
-    "name": "@rene-majewski/website-module-kreativ-register"
+    "name": "@rene-majewski/website-module-kreativ-register",
+    "configKey": "moduleKreativRegister",
+    "compatibility": {
+      "nuxt": ">=3.16.0"
+    }
   },
+  defaults: {},
   setup() {
-    console.debug("📦 @rene-majewski/website-module-kreativ-register geladen!")
+    console.info("📦 @rene-majewski/website-module-kreativ-register will be loaded")
+
+    const resolver = createResolver(import.meta.url)
+
+    addComponentsDir({path: resolver.resolve('components')})
+
+    extendPages((pages) => {
+      pages.push({
+        name: "kreativ-register",
+        path: "/kreativ-register",
+        file: resolver.resolve("runtime/pages/index.vue")
+      })
+    })
+
+    console.info(" ✓ has been loaded")
   }
 })
